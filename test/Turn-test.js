44704b0caa2efce89c55612d3698c4b1a01e3f1d
Jeffrey Cook <cookturingedu@gmail.com>
2022-07-07 14:43:00 -0400

    Create third test for turn implementation

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -13,5 +13,11 @@ describe('Turn', function() {
   it.skip('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(Turn).to.be.an.instanceof(Turn);
-  })
+  });
+
+  it.skip('should store user\'s guess as a string', function() {
+    const turn = new Turn('pug');
+    expect(turn.guess).to.be.a('string')
+
+  });
 });
