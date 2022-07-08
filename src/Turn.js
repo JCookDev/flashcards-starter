5ef14535afaeb5611eff49af7aa1bbc709a2bd2f
Jeffrey Cook <cookturingedu@gmail.com>
2022-07-08 16:55:50 -0400

    Complete Turn class implementation function and pass all tests

--- src/Turn.js
+++ src/Turn.js
@@ -11,6 +11,22 @@ class Turn {
   returnCard() {
     return this.card
   }
+
+  evaluateGuess() {
+    if (this.guess === this.card["correctAnswer"]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    if (this.evaluateGuess() === true) {
+      return `Correct!`;
+    } else {
+      return `Incorrect!`;
+    }
+  }
 }
 
 module.exports = Turn;
