a166310b5fa00e7d7163705422c6e1f43d8f5d6a
Jeffrey Cook <cookturingedu@gmail.com>
2022-07-08 16:12:52 -0400

    Create all tests for Turn implementation and refactor old tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,22 +2,39 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
+  let turn;
+
+  beforeEach(function() {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('array', card);
+  });
+
   it.skip('should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
   it.skip('should be an instance of Turn', function() {
-    const turn = new Turn();
     expect(Turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should store user\'s guess as a string', function() {
-    const turn = new Turn('pug');
-    expect(turn.guess).to.be.a('string')
-
+  it.skip('should accept a guess', function() {
+    expect(turn.returnGuess()).to.equal('array')
   });
+
+  it.skip('should return an object', function() {
+    expect(turn.returnCard()).to.equal(card)
+  });
+
+  it.skip('should evaluate guess', function() {
+    expect(turn.evaluateGuess()).to.equal(false)
+  });
+
+  it.skip('should give feedback', function() {
+    expect(turn.giveFeedback().to.equal('incorrect!'))
+  });
+  
 });
