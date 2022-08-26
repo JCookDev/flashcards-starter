e7ccd58bca4f576854f481a58a48eb1ec36d6d0e
Jeffrey Cook <cookturingedu@gmail.com>
2022-08-25 20:37:30 -0400

    Test if Game is a class constructor function

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,3 +1,17 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Game = require('../src/Game');
+
+describe('Game', function() {
+
+  let game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  });
+  
+});
