0798b2240046969640907261c07046f4c70a3d77
Jeffrey Cook <cookturingedu@gmail.com>
2022-08-25 20:56:18 -0400

    Test if Game class keeps track of the current round

--- test/Game-test.js
+++ test/Game-test.js
@@ -17,4 +17,9 @@ describe('Game', function() {
   it('should be an instance of Game', function() {
     expect(game).to.be.an.instanceof(Game);
   });
+
+  it('should be able to keep track of current round', function() {
+   game.start();
+   expect(game.currentRound).to.equal(1);
+ });
 });
