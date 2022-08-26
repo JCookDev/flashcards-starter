6b72fa90df36a174e6990e3a852d383ed65db431
Jeffrey Cook <cookturingedu@gmail.com>
2022-08-25 20:18:05 -0400

    Complete Round class constructor function implementation

---
+++ src/Round.js
@@ -0,0 +1,39 @@
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck, currentCard) {
+    this.deck = deck ? deck.cards : [];
+    this.currentCard = deck.cards[0];
+    this.turn = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+
+  takeTurn(guess) {
+    this.turn++;
+    const turn = new Turn(guess, this.currentCard);
+    this.currentCard = this.deck[this.turn];
+    if (turn.evaluateGuess() === true) {
+      return turn.giveFeedback();
+    } else {
+      this.incorrectGuesses.push(this.currentCard.id);
+      return turn.giveFeedback();
+    }
+  }
+
+  calculatePercentageCorrect() {
+    var score =
+    (((this.turn - this.incorrectGuesses.length) / this.turn) * 100);
+    return Math.floor(score);
+  }
+
+  endRound() {
+    return `** Round over! ** You answered ${this.calculatePercentageCorrect()}% of the questions correctly!`;
+  }
+
+}
+
+module.exports = Round;
